a34503c7e74ac5fca7a8d06b9889a43030773112
LAGM <luisiyi1997@gmail.com>
2025-05-11 22:00:41 -0700

    changes on front end and back end
    -Front: added a new menu item to add a new cliente with their style of prime faces, also was added a method (agregarCliente) where is called from the back thankfully for "indexControlador", then we update the windows with update attribute, and finally show a windows with "oncomplete".
    -Back: added a new attribute linked to the cliente, and we added the "agregarCliente" method for back end side, this method only initialize a new cliente.

--- src/main/java/gm/zona_fit/controlador/IndexControlador.java
+++ src/main/java/gm/zona_fit/controlador/IndexControlador.java
@@ -19,6 +19,7 @@ public class IndexControlador {
     @Autowired
     IClienteServicio clienteServicio;
     private List<Cliente> clientes;
+    private Cliente clienteSeleccionado;
     private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
 
     @PostConstruct
@@ -29,5 +30,9 @@ public class IndexControlador {
     public void cargarDatos(){
         this.clientes = this.clienteServicio.listarClientes();
         this.clientes.forEach(cliente -> logger.info(cliente.toString()));
+    }
+
+    public void agregarCliente(){
+        this.clienteSeleccionado = new Cliente();
     }
 }
