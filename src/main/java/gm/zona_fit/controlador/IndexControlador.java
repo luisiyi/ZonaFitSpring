76e4e9dbe6df89b18717108e98e86a7087f6f5b8
LAGM <luisiyi1997@gmail.com>
2025-05-15 21:35:38 -0700

    GuardarCliente method added in Front end
    -implementation of guardarCliente method in indexControlador
    -first we validate if the object comes with a not null value in the ID,
    if not we call the clienteServicio.guardarCliente from our clienteServicio
    then we show a successfully message "cliente agregado"
    after that we hide the ventanaModalCliente
    then we update our "cliente-tabla" table
    finally we set in null the clienteSeleccionado object to reuse it late.

--- src/main/java/gm/zona_fit/controlador/IndexControlador.java
+++ src/main/java/gm/zona_fit/controlador/IndexControlador.java
@@ -3,8 +3,11 @@ package gm.zona_fit.controlador;
 import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.servicio.IClienteServicio;
 import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import lombok.Data;
+import org.primefaces.PrimeFaces;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,5 +37,24 @@ public class IndexControlador {
 
     public void agregarCliente(){
         this.clienteSeleccionado = new Cliente();
+    }
+
+    public void guardarCliente(){
+        logger.info("Cliente a guardar: " + this.clienteSeleccionado);
+        //Agregar
+        if(this.clienteSeleccionado.getId() == null){
+            this.clienteServicio.guardarCliente(this.clienteSeleccionado);
+            this.clientes.add(this.clienteSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage("Cliente Agregado"));
+        }
+        //Ocultar la ventana modal
+        PrimeFaces.current().executeScript("PF('ventanaModalCliente').hide()");
+        //Actualizar la tabla usando ajax
+        PrimeFaces.current().ajax().update("forma-clientes:mensajes",
+                "forma-clientes:clientes-tabla");
+
+        //Reset del objeto cliente seleccionado
+        this.clienteSeleccionado = null;
     }
 }
